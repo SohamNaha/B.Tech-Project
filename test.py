77cb8c96453b47541a0aa3b6d69740659db49929
Soham Naha <37201714+SohamNaha@users.noreply.github.com>
2018-11-21 14:38:08 +0530

    Update test.py

--- test.py
+++ test.py
@@ -6,14 +6,26 @@ from PIL import Image
 
 # interactively ask for the .nii file
 from tkinter.filedialog import askopenfilename
-filename = askopenfilename()
+filename = askopenfilename(title="Open File")
+
+# to get folder directory
+c = '/'
+index = [pos for pos, char in enumerate(filename) if char == c]
+last = index[-1]
+directory = filename[:last]
+print(directory)
 
 #from nibabel.testing import data_path
 image = nib.load(filename)
-print(image.shape[:-1])			#image size in pixels
+print(image.shape)		      	#image size in pixels
 print(image.get_data_dtype())	#image data type
 
 # The get_data() function returns a standard NumPy multi-dimensional array, so you can treat it as you would any other ndarray.
 image_data = image.get_data()
-img = Image.fromarray(image_data)
-img
+
+for i in range(1):
+	fig = plt.figure()
+	fname = 'image'+str(i)+'.png'
+	full_path = directory + '/' + fname
+	plt.savefig(full_path , dpi=fig.dpi , bbox_inches='tight')
+	plt.imshow(image_data[:,:,i])
