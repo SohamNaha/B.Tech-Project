c0d6bbc13b0e6361c88dc52c0d419e8d776441d1
Sayantan36 <45284079+Sayantan36@users.noreply.github.com>
2018-11-26 13:18:26 +0530

    Add files via upload

--- test.py
+++ test.py
@@ -1,12 +1,16 @@
+#importing modules
 import os
 import numpy as np
 import nibabel as nib
 from  matplotlib import pyplot as plt
 from PIL import Image
-
-# interactively ask for the .nii file
+from nilearn import plotting
+from pyunpack import Archive
 from tkinter.filedialog import askopenfilename
+
+# interactively ask for the .nii.gz file
 filename = askopenfilename(title="Open File")
+print(filename)
 
 # to get folder directory
 c = '/'
@@ -15,18 +19,31 @@ last = index[-1]
 directory = filename[:last]
 print(directory)
 
-#from nibabel.testing import data_path
-image = nib.load(filename)
-a = image.shape
-print(a)		      	#image size in pixels
-print(image.get_data_dtype())	#image data type
+Archive(filename).extractall(directory)    #extracting .nii file
 
-# The get_data() function returns a standard NumPy multi-dimensional array, so you can treat it as you would any other ndarray.
+os.mkdir(filename+' images')     #creating folder to store images
+
+image = nib.load(filename)       #from nibabel.testing import data_path
+print(image.shape)
+n = image.shape			#image size in pixels
+m = n[-1]
+print(image.get_data_dtype())	#image data type
 image_data = image.get_data()
 
-for i in range(a[-1]):
-	fig = plt.figure()
-	fname = 'image'+str(i)+'.png'
-	full_path = directory + '/' + fname
-	plt.savefig(full_path , dpi=fig.dpi , bbox_inches='tight')
-	plt.imshow(image_data[:,:,i])
+
+#image sets
+for i in range(m):
+	plt.imshow(image_data[:,:,i], cmap='hot')
+	fig=plt.gcf()
+	fig.savefig(filename+' images' +'/'+ 'image' + str(i) + '.png' , bbox_inches='tight')
+
+# interactively ask for the  extracted .nii file
+filename1 = askopenfilename(title="Open File")
+
+image1 = nib.load(filename1)
+
+fig1=plotting.plot_stat_map(image1)
+fig1.savefig(filename+' images' +'/'+ 'statiscal map' + '.png')
+fig2=plotting.plot_glass_brain(image1)
+fig2.savefig(filename+' images' +'/'+ 'glass plot' + '.png')
+
